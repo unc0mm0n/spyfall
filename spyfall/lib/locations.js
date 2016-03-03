31ffbe5b8b38a68360d21655f57c9f0ad352d9c0
Evan Brumley <evan.brumley@wspdigital.com>
2016-03-03 22:34:59 +1100

    Created Spyfilm variant

--- spyfall/lib/locations.js
+++ spyfall/lib/locations.js
@@ -1,338 +1,136 @@
 locations = [
   {
-    name: 'locations.airplane',
+    name: 'locations.the force awakens',
     roles: [
-      'locations.roles.airplane.first class passenger',
-      'locations.roles.airplane.air marshall',
-      'locations.roles.airplane.mechanic',
-      'locations.roles.airplane.air hostess',
-      'locations.roles.airplane.copilot',
-      'locations.roles.airplane.captain',
-      'locations.roles.airplane.economy class passenger'
+      'locations.roles.the force awakens.rey',
+      'locations.roles.the force awakens.finn',
+      'locations.roles.the force awakens.general leia',
+      'locations.roles.the force awakens.captain phasma',
+      'locations.roles.the force awakens.poe dameron',
+      'locations.roles.the force awakens.kylo ren',
+      'locations.roles.the force awakens.bb8'
     ]
   },
   {
-    name: 'locations.bank',
+    name: 'locations.toy story',
     roles: [
-      'locations.roles.bank.armored car driver',
-      'locations.roles.bank.manager',
-      'locations.roles.bank.consultant',
-      'locations.roles.bank.robber',
-      'locations.roles.bank.security guard',
-      'locations.roles.bank.teller',
-      'locations.roles.bank.customer'
+      'locations.roles.toy story.woody',
+      'locations.roles.toy story.jessie',
+      'locations.roles.toy story.buzz lightyear',
+      'locations.roles.toy story.mr potato head',
+      'locations.roles.toy story.mrs potato head',
+      'locations.roles.toy story.little bo peep',
+      'locations.roles.toy story.slinky dog'
     ]
   },
   {
-    name: 'locations.beach',
+    name: 'locations.aliens',
     roles: [
-      'locations.roles.beach.beach waitress',
-      'locations.roles.beach.kite surfer',
-      'locations.roles.beach.lifeguard',
-      'locations.roles.beach.thief',
-      'locations.roles.beach.beach photographer',
-      'locations.roles.beach.ice cream truck driver',
-      'locations.roles.beach.beach goer'
+      'locations.roles.aliens.ripley',
+      'locations.roles.aliens.newt',
+      'locations.roles.aliens.corporal hicks',
+      'locations.roles.aliens.bishop',
+      'locations.roles.aliens.private vasquez',
+      'locations.roles.aliens.a facehugger',
+      'locations.roles.aliens.the alien queen'
     ]
   },
   {
-    name: 'locations.cathedral',
+    name: 'locations.harry potter',
     roles: [
-      'locations.roles.cathedral.priest',
-      'locations.roles.cathedral.beggar',
-      'locations.roles.cathedral.sinner',
-      'locations.roles.cathedral.tourist',
-      'locations.roles.cathedral.sponsor',
-      'locations.roles.cathedral.chorister',
-      'locations.roles.cathedral.parishioner'
+      'locations.roles.harry potter.harry potter',
+      'locations.roles.harry potter.hermione granger',
+      'locations.roles.harry potter.ron weasley',
+      'locations.roles.harry potter.ginny weasley',
+      'locations.roles.harry potter.severus snape',
+      'locations.roles.harry potter.minerva mcgonagall',
+      'locations.roles.harry potter.albus dumbledore'
     ]
   },
   {
-    name: 'locations.circus tent',
+    name: 'locations.snow white and the seven dwarfs',
     roles: [
-      'locations.roles.circus tent.acrobat',
-      'locations.roles.circus tent.animal trainer',
-      'locations.roles.circus tent.magician',
-      'locations.roles.circus tent.fire eater',
-      'locations.roles.circus tent.clown',
-      'locations.roles.circus tent.juggler',
-      'locations.roles.circus tent.visitor'
+      'locations.roles.snow white and the seven dwarfs.doc',
+      'locations.roles.snow white and the seven dwarfs.grumpy',
+      'locations.roles.snow white and the seven dwarfs.sneezy',
+      'locations.roles.snow white and the seven dwarfs.dopey',
+      'locations.roles.snow white and the seven dwarfs.happy',
+      'locations.roles.snow white and the seven dwarfs.bashful',
+      'locations.roles.snow white and the seven dwarfs.sleepy'
     ]
   },
   {
-    name: 'locations.corporate party',
+    name: 'locations.blade runner',
     roles: [
-      'locations.roles.corporate party.entertainer',
-      'locations.roles.corporate party.manager',
-      'locations.roles.corporate party.unwanted guest',
-      'locations.roles.corporate party.owner',
-      'locations.roles.corporate party.secretary',
-      'locations.roles.corporate party.delivery boy',
-      'locations.roles.corporate party.accountant'
+      'locations.roles.blade runner.deckard',
+      'locations.roles.blade runner.batty',
+      'locations.roles.blade runner.priss',
+      'locations.roles.blade runner.rachael',
+      'locations.roles.blade runner.gaff',
+      'locations.roles.blade runner.leon',
+      'locations.roles.blade runner.zhora'
     ]
   },
   {
-    name: 'locations.crusader army',
+    name: 'locations.back to the future',
     roles: [
-      'locations.roles.crusader army.monk',
-      'locations.roles.crusader army.imprisoned saracen',
-      'locations.roles.crusader army.servant',
-      'locations.roles.crusader army.bishop',
-      'locations.roles.crusader army.squire',
-      'locations.roles.crusader army.archer',
-      'locations.roles.crusader army.knight'
+      'locations.roles.back to the future.marty mcfly',
+      'locations.roles.back to the future.jennifer parker',
+      'locations.roles.back to the future.dr emmett brown',
+      'locations.roles.back to the future.biff tannen',
+      'locations.roles.back to the future.lorraine baines',
+      'locations.roles.back to the future.george mcfly',
+      'locations.roles.back to the future.einstein the dog'
     ]
   },
   {
-    name: 'locations.casino',
+    name: 'locations.the princess bride',
     roles: [
-      'locations.roles.casino.bartender',
-      'locations.roles.casino.head security guard',
-      'locations.roles.casino.bouncer',
-      'locations.roles.casino.manager',
-      'locations.roles.casino.hustler',
-      'locations.roles.casino.dealer',
-      'locations.roles.casino.gambler'
+      'locations.roles.the princess bride.westley',
+      'locations.roles.the princess bride.buttercup',
+      'locations.roles.the princess bride.inigo montoya',
+      'locations.roles.the princess bride.fezzik',
+      'locations.roles.the princess bride.vizzini',
+      'locations.roles.the princess bride.miracle max',
+      'locations.roles.the princess bride.prince humperdinck'
     ]
   },
   {
-    name: 'locations.day spa',
+    name: 'locations.ghostbusters',
     roles: [
-      'locations.roles.day spa.stylist',
-      'locations.roles.day spa.masseuse',
-      'locations.roles.day spa.manicurist',
-      'locations.roles.day spa.makeup artist',
-      'locations.roles.day spa.dermatologist',
-      'locations.roles.day spa.beautician',
-      'locations.roles.day spa.customer'
+      'locations.roles.ghostbusters.peter venkman',
+      'locations.roles.ghostbusters.egon spengler',
+      'locations.roles.ghostbusters.winston zeddemore',
+      'locations.roles.ghostbusters.ray stantz',
+      'locations.roles.ghostbusters.janine',
+      'locations.roles.ghostbusters.dana barrett',
+      'locations.roles.ghostbusters.slimer'
     ]
   },
   {
-    name: 'locations.embassy',
+    name: 'locations.die hard',
     roles: [
-      'locations.roles.embassy.security guard',
-      'locations.roles.embassy.secretary',
-      'locations.roles.embassy.ambassador',
-      'locations.roles.embassy.tourist',
-      'locations.roles.embassy.refugee',
-      'locations.roles.embassy.diplomat',
-      'locations.roles.embassy.government official'
+      'locations.roles.die hard.john mclane',
+      'locations.roles.die hard.hans gruber',
+      'locations.roles.die hard.a terrorist'
     ]
   },
   {
-    name: 'locations.hospital',
+    name: 'locations.the wizard of oz',
     roles: [
-      'locations.roles.hospital.nurse',
-      'locations.roles.hospital.doctor',
-      'locations.roles.hospital.anesthesiologist',
-      'locations.roles.hospital.intern',
-      'locations.roles.hospital.therapist',
-      'locations.roles.hospital.surgeon',
-      'locations.roles.hospital.patient'
+      'locations.roles.the wizard of oz.dorothy',
+      'locations.roles.the wizard of oz.glinda',
+      'locations.roles.the wizard of oz.the tin man',
+      'locations.roles.the wizard of oz.the lion',
+      'locations.roles.the wizard of oz.the scarecrow',
+      'locations.roles.the wizard of oz.toto',
+      'locations.roles.the wizard of oz.the wicked witch of the west'
     ]
   },
   {
-    name: 'locations.hotel',
+    name: 'locations.being john malkovich',
     roles: [
-      'locations.roles.hotel.doorman',
-      'locations.roles.hotel.security guard',
-      'locations.roles.hotel.manager',
-      'locations.roles.hotel.housekeeper',
-      'locations.roles.hotel.bartender',
-      'locations.roles.hotel.bellman',
-      'locations.roles.hotel.customer'
-    ]
-  },
-  {
-    name: 'locations.military base',
-    roles: [
-      'locations.roles.military base.deserter',
-      'locations.roles.military base.colonel',
-      'locations.roles.military base.medic',
-      'locations.roles.military base.sniper',
-      'locations.roles.military base.officer',
-      'locations.roles.military base.tank engineer',
-      'locations.roles.military base.soldier'
-    ]
-  },
-  {
-    name: 'locations.movie studio',
-    roles: [
-      'locations.roles.movie studio.stunt man',
-      'locations.roles.movie studio.sound engineer',
-      'locations.roles.movie studio.camera man',
-      'locations.roles.movie studio.director',
-      'locations.roles.movie studio.costume artist',
-      'locations.roles.movie studio.producer',
-      'locations.roles.movie studio.actor'
-    ]
-  },
-  {
-    name: 'locations.ocean liner',
-    roles: [
-      'locations.roles.ocean liner.cook',
-      'locations.roles.ocean liner.captain',
-      'locations.roles.ocean liner.bartender',
-      'locations.roles.ocean liner.musician',
-      'locations.roles.ocean liner.waiter',
-      'locations.roles.ocean liner.mechanic',
-      'locations.roles.ocean liner.rich passenger'
-    ]
-  },
-  {
-    name: 'locations.passenger train',
-    roles: [
-      'locations.roles.passenger train.mechanic',
-      'locations.roles.passenger train.border patrol',
-      'locations.roles.passenger train.train attendant',
-      'locations.roles.passenger train.restaurant chef',
-      'locations.roles.passenger train.train driver',
-      'locations.roles.passenger train.stoker',
-      'locations.roles.passenger train.passenger'
-    ]
-  },
-  {
-    name: 'locations.pirate ship',
-    roles: [
-      'locations.roles.pirate ship.cook',
-      'locations.roles.pirate ship.slave',
-      'locations.roles.pirate ship.cannoneer',
-      'locations.roles.pirate ship.tied up prisoner',
-      'locations.roles.pirate ship.cabin boy',
-      'locations.roles.pirate ship.brave captain',
-      'locations.roles.pirate ship.sailor'
-    ]
-  },
-  {
-    name: 'locations.polar station',
-    roles: [
-      'locations.roles.polar station.medic',
-      'locations.roles.polar station.expedition leader',
-      'locations.roles.polar station.biologist',
-      'locations.roles.polar station.radioman',
-      'locations.roles.polar station.hydrologist',
-      'locations.roles.polar station.meteorologist',
-      'locations.roles.polar station.geologist'
-    ]
-  },
-  {
-    name: 'locations.police station',
-    roles: [
-      'locations.roles.police station.detective',
-      'locations.roles.police station.lawyer',
-      'locations.roles.police station.journalist',
-      'locations.roles.police station.criminalist',
-      'locations.roles.police station.archivist',
-      'locations.roles.police station.criminal',
-      'locations.roles.police station.patrol officer'
-    ]
-  },
-  {
-    name: 'locations.restaurant',
-    roles: [
-      'locations.roles.restaurant.musician',
-      'locations.roles.restaurant.bouncer',
-      'locations.roles.restaurant.hostess',
-      'locations.roles.restaurant.head chef',
-      'locations.roles.restaurant.food critic',
-      'locations.roles.restaurant.waiter',
-      'locations.roles.restaurant.customer'
-    ]
-  },
-  {
-    name: 'locations.school',
-    roles: [
-      'locations.roles.school.gym teacher',
-      'locations.roles.school.principal',
-      'locations.roles.school.security guard',
-      'locations.roles.school.janitor',
-      'locations.roles.school.cafeteria lady',
-      'locations.roles.school.maintenance man',
-      'locations.roles.school.student'
-    ]
-  },
-  {
-    name: 'locations.service station',
-    roles: [
-      'locations.roles.service station.manager',
-      'locations.roles.service station.tire specialist',
-      'locations.roles.service station.biker',
-      'locations.roles.service station.car owner',
-      'locations.roles.service station.car wash operator',
-      'locations.roles.service station.electrician',
-      'locations.roles.service station.auto mechanic'
-    ]
-  },
-  {
-    name: 'locations.space station',
-    roles: [
-      'locations.roles.space station.engineer',
-      'locations.roles.space station.alien',
-      'locations.roles.space station.pilot',
-      'locations.roles.space station.commander',
-      'locations.roles.space station.scientist',
-      'locations.roles.space station.doctor',
-      'locations.roles.space station.space tourist'
-    ]
-  },
-  {
-    name: 'locations.submarine',
-    roles: [
-      'locations.roles.submarine.cook',
-      'locations.roles.submarine.commander',
-      'locations.roles.submarine.sonar technician',
-      'locations.roles.submarine.electronics technician',
-      'locations.roles.submarine.radioman',
-      'locations.roles.submarine.navigator',
-      'locations.roles.submarine.sailor'
-    ]
-  },
-  {
-    name: 'locations.supermarket',
-    roles: [
-      'locations.roles.supermarket.cashier',
-      'locations.roles.supermarket.butcher',
-      'locations.roles.supermarket.janitor',
-      'locations.roles.supermarket.security guard',
-      'locations.roles.supermarket.food sample demonstrator',
-      'locations.roles.supermarket.shelf stocker',
-      'locations.roles.supermarket.customer'
-    ]
-  },
-  {
-    name: 'locations.theater',
-    roles: [
-      'locations.roles.theater.coat check lady',
-      'locations.roles.theater.prompter',
-      'locations.roles.theater.cashier',
-      'locations.roles.theater.director',
-      'locations.roles.theater.actor',
-      'locations.roles.theater.crew man',
-      'locations.roles.theater.audience member'
-    ]
-  },
-  {
-    name: 'locations.university',
-    roles: [
-      'locations.roles.university.graduate student',
-      'locations.roles.university.professor',
-      'locations.roles.university.dean',
-      'locations.roles.university.psychologist',
-      'locations.roles.university.maintenance man',
-      'locations.roles.university.janitor',
-      'locations.roles.university.student'
-    ]
-  },
-  {
-    name: 'locations.world war ii squad',
-    roles: [
-      'locations.roles.world war ii squad.resistance fighter',
-      'locations.roles.world war ii squad.radioman',
-      'locations.roles.world war ii squad.scout',
-      'locations.roles.world war ii squad.medic',
-      'locations.roles.world war ii squad.cook',
-      'locations.roles.world war ii squad.imprisoned nazi',
-      'locations.roles.world war ii squad.soldier'
+      'locations.roles.being john malkovich.john malkovich'
     ]
   }
 ];
